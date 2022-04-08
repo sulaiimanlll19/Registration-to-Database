afaa278efc9863226d43318a861d214340713287
sulaiman <sulaiimanlll19@gmail.com>
2022-04-09 01:45:48 +0300

    Linking with database code

--- connect.php
+++ connect.php
@@ -1,20 +1,22 @@
 <?php
 
-    $email = $_POST['email'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+    $email = $_POST['email']; // The attribute NAME in the HTML code should same with POST method
+    $username = $_POST['username']; // The attribute NAME in the HTML code should same with POST method
+    $password = $_POST['password']; // The attribute NAME in the HTML code should same with POST method
 
+    // Checking if fields of regiatrtion are empty or not
     if( !empty($email) || !empty($username) || !empty($password) ){
 
         // Create connection
-        $conn = new mysqli('localhost','root','','game');
+        $conn = new mysqli('localhost','root','','game'); // default attributes --> 'game' is my database name
 
+        // Checking connection
         if(mysqli_connect_error()){
             die('Connect error ( '. mysqli_connect_errno().' )'. mysqli_connect_error());
         }else{
 
-            $SELECT = "SELECT email From users Where email = ? Limit 1";
-            $INSERT = "INSERT Into users (email, username, password) values(?, ?, ?)";
+            $SELECT = "SELECT email From users Where email = ? Limit 1"; // Limiting the email registration to ONE to not repeating
+            $INSERT = "INSERT Into users (email, username, password) values(?, ?, ?)"; // Inserting values into my 'users' table -- each '?' represent a variable
 
             // prepare steatement
             $stmt = $conn->prepare($SELECT);
@@ -24,49 +26,24 @@
             $stmt->store_result();
             $rnum = $stmt->num_rows;
 
-            if( $rnum == 0 ){
+            if( $rnum == 0 ){ // Checking if email already registered or not
                 $stmt->close();
 
                 $stmt = $conn->prepare($INSERT);
                 $stmt->bind_param("sss", $email, $username, $password);
                 $stmt->execute();
                 echo 'New record Successfully...';
-            }else{
+            }else{ // if yes then send this message
                 echo 'This email Already registered!!!';
             }
-            $stmt->close();
-            $conn->close();
+            $stmt->close(); // Close database
+            $conn->close(); // Close connection
         }
 
 
-    }else{
+    }else{ // if field empty send this meesage
         echo 'All fields are required!';
         die();
     }
-
-
-
-
-    // Working Code but WITHOUT checking if registered or not
-
-
-    // $email = $_POST['email'];
-    // $username = $_POST['username'];
-    // $password = $_POST['password'];
-
-    // // Database connection
-    // $conn = new mysqli('localhost','root','','game');
-    // if($conn->connect_error){
-
-    //     die('Connection Failed : '.$conn->connect_error);
-    // }else{
-
-    //     $stmt = $conn->prepare("insert into users(email, username, password) values(?, ?, ?)");
-    //     $stmt->bind_param("sss", $email, $username, $password);
-    //     $stmt->execute();
-    //     echo "Registration Successfully...";
-    //     $stmt->close();
-    //     $conn->close();
-    // }
 
 ?>
